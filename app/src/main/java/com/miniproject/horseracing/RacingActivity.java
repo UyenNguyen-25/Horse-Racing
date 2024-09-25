6184833c9eb8156e3655e1ac5047af0e43705b85
UyenNguyen-25 <khoang8666@gmail.com>
2024-09-26 00:38:51 +0700

    updated

--- app/src/main/java/com/miniproject/horseracing/RacingActivity.java
+++ app/src/main/java/com/miniproject/horseracing/RacingActivity.java
@@ -1,14 +1,82 @@
 package com.miniproject.horseracing;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class RacingActivity extends AppCompatActivity {
+public class RacingActivity extends AppCompatActivity implements View.OnClickListener {
+    Handler handler=new Handler();
+    int finishTime = 6000;
+    Button startBtn, resetBtn, logoutBtn;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_racing);
+
+        logoutBtn = findViewById(R.id.btnLogout);
+        logoutBtn.setOnClickListener(this);
+
+        startBtn = findViewById(R.id.btnStart);
+        startBtn.setOnClickListener(this);
+
+        resetBtn = findViewById(R.id.BtnReset);
+        resetBtn.setOnClickListener(this);
+    }
+
+    void onMusicStart(int delay){
+        MediaPlayer music = MediaPlayer.create(RacingActivity.this, R.raw.horse_sound);
+        music.start();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.d("replay", "replay: Success");
+                music.stop();
+            }
+        }, delay);
+    }
+
+    void onRacingStart(){
+        onMusicStart(finishTime);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+            onRacingFinish();
+            }
+        },finishTime);
+    }
+
+    void onRacingFinish(){
+        Log.d("Finish", "onRacingFinish: Success");
+        Intent intent = new Intent(this, ResultActivity.class);
+        intent.putExtra("horse","HeHe");
+        intent.putExtra("reward",50);
+        startActivity(intent);
+    }
+
+    void onReset(){
+        Log.d("ResetBtnReset", "onReset: Success");
+    }
+
+    void onLogout(){
+        Intent intent = new Intent(this,  MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.btnStart){
+            onRacingStart();
+        }else if(view.getId() == R.id.btnLogout){
+            onLogout();
+        }else if(view.getId() == R.id.BtnReset){
+            onReset();
+        }
     }
 }
--- app/src/main/java/com/miniproject/horseracing/ResultActivity.java
+++ app/src/main/java/com/miniproject/horseracing/ResultActivity.java
@@ -1,6 +1,8 @@
 package com.miniproject.horseracing;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +12,10 @@ public class ResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_result);
+        Intent intent = getIntent();
+        String horse = intent.getStringExtra("horse");
+        Log.d("winner", "winner:"+horse);
+        int reward = intent.getIntExtra("reward",0);
+        Log.d("reward", "reward:"+reward);
     }
 }
