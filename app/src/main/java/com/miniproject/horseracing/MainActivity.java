1dee1e809d13968ea280598c051e18ad4bc361b0
srx <SeraphUltimateX@gmail.com>
2024-09-27 22:57:33 +0700

    TEMP

--- app/src/main/java/com/miniproject/horseracing/MainActivity.java
+++ app/src/main/java/com/miniproject/horseracing/MainActivity.java
@@ -13,12 +13,10 @@ import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-import model.Account;
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     public final static String ERROR_MESSAGE = "Please enter full fill";
 
-    EditText  usernameTxt, passwordTxt;
+    EditText usernameTxt, passwordTxt;
     Button signInBtn;
     TextView usernameErr, passwordErr;
 
@@ -28,8 +26,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        usernameTxt =findViewById(R.id.UsernameEditText);
-        passwordTxt =findViewById(R.id.PasswordEditText);
+        usernameTxt = findViewById(R.id.UsernameEditText);
+        passwordTxt = findViewById(R.id.PasswordEditText);
         usernameErr = findViewById(R.id.usernameError);
         passwordErr = findViewById(R.id.passwordError);
         signInBtn = findViewById(R.id.LoginButton);
@@ -39,25 +37,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     boolean checkInput() {
         boolean isValidate = true;
-        if (TextUtils.isEmpty(usernameTxt.getText().toString()) ) {
+        if (TextUtils.isEmpty(usernameTxt.getText().toString())) {
             usernameErr.setText(ERROR_MESSAGE);
             usernameErr.setVisibility(View.VISIBLE);
             isValidate = false;
-        }else {
+        } else {
             usernameErr.setVisibility(View.GONE);
         }
-        if (TextUtils.isEmpty(passwordTxt.getText().toString()) ) {
+        if (TextUtils.isEmpty(passwordTxt.getText().toString())) {
             passwordErr.setText(ERROR_MESSAGE);
             passwordErr.setVisibility(View.VISIBLE);
             isValidate = false;
-        }else {
+        } else {
             passwordErr.setVisibility(View.GONE);
-        };
+        }
+        ;
         return isValidate;
     }
 
-    void signIn (){
-        if (!checkInput()){
+    void signIn() {
+        if (!checkInput()) {
             return;
         }
         Log.d("Success", "signIn: sign in success");
@@ -67,7 +66,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        if(view.getId() == R.id.LoginButton){
+        if (view.getId() == R.id.LoginButton) {
             signIn();
         }
     }
--- app/src/main/java/com/miniproject/horseracing/ResultActivity.java
+++ app/src/main/java/com/miniproject/horseracing/ResultActivity.java
@@ -1,14 +1,87 @@
 package com.miniproject.horseracing;
 
-import android.os.Bundle;
+import static android.content.ContentValues.TAG;
 
-import androidx.activity.EdgeToEdge;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ResultActivity extends AppCompatActivity {
+    TextView tvWinningHorse, tvHorse1Result, tvHorse2Result, tvHorse3Result;
+    TextView tvBalance, tvReward, tvHorse1Bet, tvHorse2Bet, tvHorse3Bet;
+    Button btnPlayAgain, btnReset;
+    ImageView imgHorse1, imgHorse2, imgHorse3;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_result);
+
+        tvWinningHorse = findViewById(R.id.tvWinningHorse);
+        tvHorse1Result = findViewById(R.id.tvHorse1Result);
+        tvHorse2Result = findViewById(R.id.tvHorse2Result);
+        tvHorse3Result = findViewById(R.id.tvHorse3Result);
+        tvBalance = findViewById(R.id.tvBalance);
+        tvReward = findViewById(R.id.tvReward);
+        btnPlayAgain = findViewById(R.id.btnPlayAgain);
+        btnReset = findViewById(R.id.btnReset);
+
+        tvHorse1Bet = findViewById(R.id.tvHorse1Bet);
+        tvHorse2Bet = findViewById(R.id.tvHorse2Bet);
+        tvHorse3Bet = findViewById(R.id.tvHorse3Bet);
+
+        imgHorse1 = findViewById(R.id.imgHorse1);
+        imgHorse2 = findViewById(R.id.imgHorse2);
+        imgHorse3 = findViewById(R.id.imgHorse3);
+
+        int[] standings = getIntent().getIntArrayExtra("standings");
+        double balanceValue = getIntent().getDoubleExtra("balance", 0);
+        double bet1 = getIntent().getDoubleExtra("BET1", 0);
+        double bet2 = getIntent().getDoubleExtra("BET2", 0);
+        double bet3 = getIntent().getDoubleExtra("BET3", 0);
+        double ODDS = getIntent().getDoubleExtra("odds", 0);
+        double total = getIntent().getDoubleExtra("total", 0);
+        Log.d(TAG, "balanceValue: " + balanceValue);
+        Log.d(TAG, "total: " + total);
+
+        tvBalance.setText(String.format("%.0f", balanceValue));
+        tvReward.setText(String.format("%.0f", total));
+
+        if (standings[0] == 1) {
+            tvWinningHorse.setText("Horse 1 Wins!");
+            imgHorse1.setImageResource(R.drawable.horse_2);
+            imgHorse2.setImageResource(R.drawable.horse_1);
+            imgHorse3.setImageResource(R.drawable.horse_3);
+            tvHorse2Bet.setText(String.format("%.0f", bet1));
+            tvHorse1Bet.setText(String.format("%.0f", bet2));
+            tvHorse3Bet.setText(String.format("%.0f", bet3));
+        } else if (standings[1] == 1) {
+            tvWinningHorse.setText("Horse 2 Wins!");
+            imgHorse1.setImageResource(R.drawable.horse_1);
+            imgHorse2.setImageResource(R.drawable.horse_2);
+            imgHorse3.setImageResource(R.drawable.horse_3);
+            tvHorse2Bet.setText(String.format("%.0f", bet2));
+            tvHorse1Bet.setText(String.format("%.0f", bet1));
+            tvHorse3Bet.setText(String.format("%.0f", bet3));
+        } else if (standings[2] == 1) {
+            tvWinningHorse.setText("Horse 3 Wins!");
+            imgHorse1.setImageResource(R.drawable.horse_1);
+            imgHorse2.setImageResource(R.drawable.horse_3);
+            imgHorse3.setImageResource(R.drawable.horse_2);
+            tvHorse2Bet.setText(String.format("%.0f", bet3));
+            tvHorse1Bet.setText(String.format("%.0f", bet1));
+            tvHorse3Bet.setText(String.format("%.0f", bet2));
+        }
+
+        btnPlayAgain.setOnClickListener(v -> {
+            finish();
+        });
+        btnReset.setOnClickListener(v -> {
+            finish();
+        });
     }
 }
